7966b0d0c3fead9141d442414ccdfc458e69f455
nyuZyjCs <qq1219637652@gmail.com>
2024-04-30 20:40:00 -0400

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,130 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  XIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/portrait.jpg'
+
+function SocialLink({
+  className,
+  href,
+  children,
+  icon: Icon,
+}: {
+  className?: string
+  href: string
+  icon: React.ComponentType<{ className?: string }>
+  children: React.ReactNode
+}) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'About',
+  description:
+    'I’m Yingjie Zhang, a master student majored in computer engineering.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            I'm Yingjie Zhang, a computer enthusiast.
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+              Starting in high school, I was obsessed with mathematics. Unlike 
+              many of my peers who struggled and often felt confused by mathematics,
+              I thrived. I felt confident and found joy in it.
+            </p>
+            <p>
+              Upon entering university, my passion for mathematics naturally 
+              drew me towards machine learning. At that time, deep learning 
+              was already popular. Eager to explore, I joined my professor's 
+              lab to research deep learning for helping patients with Alzheimer's 
+              disease.Additionally, I participated in Kaggle competitions, 
+              winning both gold and silver medals.
+            </p>
+            <p>
+              Beyond machine learning, what brings me even more joy is web development
+              and learning algorithms. Although I had no prior experience in these 
+              areas during my undergraduate studies, I now devote nearly all my time
+              to mastering algorithms and building websites. This self-driven pursuit
+              has not only expanded my skills but also fueled my passion for technology.
+            </p>
+            <p>
+              Currently, I am actively seeking a software development engineer role to
+              further explore exciting technologies. Though I haven't yet got an interview
+              and the job market is still challenging, I'm not discouraged. I believe 
+              perseverance pays off, and I'm committed to preparing myself thoroughly to
+              be the best candidate possible.
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+            <SocialLink href="#" icon={XIcon}>
+              Follow on X
+            </SocialLink>
+            <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+              Follow on Instagram
+            </SocialLink>
+            <SocialLink href="https://github.com/nyuZyjCs" icon={GitHubIcon} className="mt-4">
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink href="https://www.linkedin.com/in/yingjiezhang6394/" icon={LinkedInIcon} className="mt-4">
+              Follow on LinkedIn
+            </SocialLink>
+            <SocialLink
+              href="mailto:yingjiezhang@nyu.edu"
+              icon={MailIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
+              yingjiezhang@nyu.edu
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
